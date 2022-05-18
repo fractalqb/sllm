03d73c70c600e6a726230c42affcaddcbf927aa2
Marcus <marcus@fractalqb.de>
2022-05-18 19:57:46 +0200

    Update to Go 1.18 and add fuzz tests

--- go.mod
+++ go.mod
@@ -1,7 +1,5 @@
 module git.fractalqb.de/fractalqb/sllm
 
-go 1.13
+go 1.18
 
-retract (
-	v1.0.0 // Published accidentally.
-)
+retract v1.0.0 // Published accidentally.
--- wrarg_test.go
+++ wrarg_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"log"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -35,6 +36,41 @@ func ExampleMap_undef() {
 	}))
 	// Output:
 	// just an `what:example`: `miss:<undef>`
+}
+
+func fuzzArgs[T any](t *testing.T, arg T) {
+	const tmpl = "Just `a` template"
+	var sb strings.Builder
+	n, err := Expand(&sb, tmpl, Args(nil, arg))
+	if err != nil {
+		t.Error(err)
+	}
+	if n != sb.Len() {
+		t.Errorf("Output len %d; written %d byte", sb.Len(), n)
+	}
+	msg := sb.String()
+	if !strings.HasPrefix(msg, "Just `a:") {
+		t.Errorf("Message does not start properly")
+	}
+	if !strings.HasSuffix(msg, "` template") {
+		t.Errorf("Message does not end properly")
+	}
+}
+
+func FuzzExpand_stringArgs(f *testing.F) {
+	f.Add("thing")
+	f.Add("")
+	f.Add(" łäü")
+	f.Add("just a rather long piece of content")
+	f.Fuzz(func(t *testing.T, arg string) { fuzzArgs(t, arg) })
+}
+
+func FuzzExpand_intArgs(f *testing.F) {
+	f.Add(0)
+	f.Add(-1)
+	f.Add(1)
+	f.Add(4711)
+	f.Fuzz(func(t *testing.T, arg int) { fuzzArgs(t, arg) })
 }
 
 func BenchmarkStdLog(b *testing.B) {
